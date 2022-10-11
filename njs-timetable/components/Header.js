931362e6a1cce042a0ac90288f475129adede221
Trigon25 <marccherndiyong2528@gmail.com>
2022-10-11 17:32:54 +0800

    update header css

--- njs-timetable/components/Header.js
+++ njs-timetable/components/Header.js
@@ -2,7 +2,7 @@
 
 const Header = (props) => {
     return (
-        <div className="bg-sky-800 w-screen h-20 flex justify-center items-center">
+        <div className="bg-rose-400 w-screen h-20 flex justify-center items-center">
             <p className="text-white text-3xl font-bold">{props.title}</p>
         </div>
     )
