a7f2ecde563f1f1a916638fda1d8eefaed2eefa7
Trigon25 <marccherndiyong2528@gmail.com>
2022-10-11 16:55:27 +0800

    update timeable slots formatting

--- njs-timetable/components/Header.js
+++ njs-timetable/components/Header.js
@@ -2,8 +2,8 @@
 
 const Header = (props) => {
     return (
-        <div className="bg-sky-800 w-screen h-28 flex justify-center items-center">
-            <p className="text-white text-3xl">{props.title}</p>
+        <div className="bg-sky-800 w-screen h-20 flex justify-center items-center">
+            <p className="text-white text-3xl font-bold">{props.title}</p>
         </div>
     )
 }
--- njs-timetable/pages/index.js
+++ njs-timetable/pages/index.js
@@ -1,40 +1,66 @@
-import Header from "../components/Header.js"
+import React from "react"
+import { render } from "react-dom"
+import Header from "../components/Header"
+import DaySched from "../components/DaySched"
 
-
-
+let timetable = require('../data/timetable.json');
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-const sem = [
-  { name: 'Semester One', href: '#', current: false },
-  { name: 'Semester Two', href: '#', current: false },
-]
-
-
 const Home = () => {
+  const [isCurrent, setIsCurrent] = React.useState('false')
+  const toggleCurrent = () => { setIsCurrent(prevToggle => !prevToggle) }
+
+  const [Data, setData] = React.useState(timetable)
+
+  const sem = { name: 'Show Timetable', current: isCurrent }
   return (
     <div className="flex min-h-screen flex-col items-center">
       <Header title="Timetable" />
-      <div className="flex-row items-center">
-        {sem.map((item) => (
-          <a
-            key={item.name}
-            href={item.href}
-            className={classNames(
-              item.current
-                ? 'bg-gray-900 text-white'
-                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-              'px-3 py-2 rounded-md text-sm font-medium'
-            )}
-          >
-            {item.name}
-          </a>
-        ))}
+      <div className="flex-col items-center">
+        <button
+          onClick={toggleCurrent}
+          className={classNames(
+            sem.current
+              ? 'bg-gray-900 text-white'
+              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+            'px-3 py-2 rounded-md text-sm font-medium'
+          )}
+        >
+          {sem.name}
+        </button> 
       </div>
+      {isCurrent ?
+        <div className="grid grid-cols-5 gap-1 py-4" >
+          <DaySched day="Mon" slots={Data[0].data[0].slots} />
+          <DaySched day="Tue" slots={Data[0].data[1].slots} />
+          <DaySched day="Wed" slots={Data[0].data[2].slots} />
+          <DaySched day="Thu" slots={Data[0].data[3].slots} />
+          <DaySched day="Fri" slots={Data[0].data[4].slots} />
+          {/* <DaySched day="Sat" slots={Data[0].data[5].slots} />
+          <DaySched day="Sun" slots={Data[0].data[6].slots} /> */}
+        </div>
+        : null
+      }
     </div>
   )
 }
 
 export default Home;
+
+
+{/* <a
+  key={item.name}
+  href={item.href}
+  className={classNames(
+    item.current
+      ? 'bg-gray-900 text-white'
+      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+    'px-3 py-2 rounded-md text-sm font-medium'
+  )}
+  aria-current={item.current ? `${item.name}` : undefined}
+>
+  {item.name}
+</a> */}
